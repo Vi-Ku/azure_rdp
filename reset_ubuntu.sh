7e6f4cbd873c002c60a286d3a0980f8141f3a2ed
Vishal Kumar <mpd18i027@iiitdm.ac.in>
2020-06-26 16:34:53 +0530

    Create reset_ubuntu.sh
    command to reset ubuntu automatically

---
+++ reset_ubuntu.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo dpkg --configure -a
+sudo apt-get update
+sudo apt-get -f install
+sudo apt-get full-upgrade
+sudo apt-get install --reinstall ubuntu-desktop
+sudo apt-get autoremove
+sudo apt-get clean
+sudo reboot
