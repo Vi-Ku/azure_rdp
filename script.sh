eca9a54d6cb48b6bbbb128423b3840be6b4dd969
Vishal Kumar <mpd18i027@iiitdm.ac.in>
2020-06-27 15:29:11 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,8 +6,8 @@ sudo update-alternatives --config gdm3.css
 sudo systemctl enable xrdp 
 sudo service xrdp restart 
 sudo apt update 
-sudo apt -y upgrade &&
-sudo apt update &&
+sudo apt -y upgrade 
+sudo apt update 
 sudo apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates 
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - 
 sudo apt -y install nodejs 
