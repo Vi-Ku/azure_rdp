f8ae2538458f49d7f74d451ecb94a74948060806
Vishal Kumar <mpd18i027@iiitdm.ac.in>
2020-07-25 10:26:04 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,10 +6,6 @@ sudo /etc/init.d/xrdp start
 sudo apt update 
 sudo apt -y upgrade 
 sudo apt update 
-sudo apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates 
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - 
-sudo apt -y install nodejs 
-sudo npm install -g udacimak --unsafe-perm=true --allow-root
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb 
 sudo apt install -y ./google-chrome-stable_current_amd64.deb
 sudo ln -s /usr/bin/python3 /usr/local/bin/python
